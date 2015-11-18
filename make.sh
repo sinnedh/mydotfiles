5f6aee10b5fe1319c9979de08f8265bd203c384c
Dennis Hellmich <dennis.hellmich@orderbird.com>
2015-11-18 18:16:14 +0100

    Add install script

---
+++ make.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+DIR=~/mydotfiles                    # dotfiles directory
+OLDDIR=~/mydotfiles_old             # old dotfiles backup directory
+FILES="bashrc vimrc"    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $OLDDIR for backup of any existing dotfiles in ~"
+mkdir -p $OLDDIR
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $DIR directory"
+cd $DIR
+echo "...done"
+
+# move and create symlinks
+for FILE in $FILES; do
+    echo "Moving any existing dotfiles from ~ to $OLDDIR"
+    mv ~/.$FILE $OLDDIR
+    echo "Creating symlink to $FILE in home directory."
+    ln -s $DIR/$FILE ~/.$FILE
+done
